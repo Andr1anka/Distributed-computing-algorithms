b187bd3b6da07bf0176309b8e33d0ce4e3b90c1c
mterletskyi <mykola.terletskyi@consensus.com>
2026-02-24 12:27:42 +0200

    Tarry

--- scheduler/implementation/current_network.py
+++ scheduler/implementation/current_network.py
@@ -1,8 +1,9 @@
 import uuid
+from pprint import pprint
 from typing import List, Dict
 
 from scheduler.abstract.abstract_network import AbstractNetwork
-from scheduler.implementation.node import Node
+from scheduler.implementation.tarry_node import TarryNode
 
 
 class CurrentNetwork(AbstractNetwork):
@@ -10,10 +11,11 @@ class CurrentNetwork(AbstractNetwork):
 
     def __init__(self) -> None:
         self.nodes = []
-        ids = [uuid.uuid4() for _ in range(self.NUMBER_OF_NODES)]
+        # ids = [uuid.uuid4() for _ in range(self.NUMBER_OF_NODES)]
+        ids = [f"Node-{i}" for i in range(self.NUMBER_OF_NODES)]
         self.__get_edges(ids)
         for node_id in ids:
-            self.nodes.append(Node(node_id, self.edges[node_id]))
+            self.nodes.append(TarryNode(node_id, self.edges[node_id]))
         super().__init__(self.nodes)
 
     def __get_edges(self, ids: List[uuid.UUID]) -> Dict[uuid.UUID, List[uuid.UUID]]:
@@ -27,4 +29,5 @@ class CurrentNetwork(AbstractNetwork):
             ids[6]: [ids[2]],
             ids[7]: [ids[2]]
         }
+        print(self.edges)
         return self.edges
